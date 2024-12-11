69936088eb255cb52016579bf978e97c31063837
Vetri Selvan M <123564214+vetriking1@users.noreply.github.com>
2024-12-11 21:04:38 +0530

    Update Hero.tsx 2

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -36,7 +36,7 @@ const Hero = () => {
             </p>
 
             <div className="flex space-x-6">
-              <a href="/vsmresume.pdf" download target="_blank"  className="btn-primary">
+              <a href="https://github.com/vetriking1/Portfolio/blob/main/public/vsmresume.pdf" download target="_blank"  className="btn-primary">
                 <Download className="w-5 h-5 mr-2" />
                 Download CV
               </a>
